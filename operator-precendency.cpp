704b1bd4e2d4ac03425e3ddc43d060835bab6925
Drubo Roy Bishwas <drb3368737@gmail.com>
2023-09-02 22:38:54 +0600

    added few programs

---
+++ operator-precendency.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // int i=10;
+    // while (i--)
+    // {
+    //     cout<<i;
+    // }
+
+    // while (i)
+    // {
+    //     i--;
+    //     cout<<i;
+    // }
+    
+
+    int a = 2;
+    int b= a+1;
+    if (a=4==b)
+    {
+        cout<<a<<endl;
+    }
+    else{
+        cout<<a+1;
+    }
+    // int x=4;
+    // int x=5;
+    // cout<<x;
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     for (int i = 0; i < 5; i++)
+    //     {
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+        
+    // }
+    
+    
+    return 0;
+}
